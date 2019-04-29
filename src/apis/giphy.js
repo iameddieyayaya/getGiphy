37ecefa58587de3e34385ee26e1289647fa08536
Eddie Gomez <eddieegomez4@gmail.com>
2019-04-29 11:45:18 -0700

    changed baseURL to https

--- src/apis/giphy.js
+++ src/apis/giphy.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const KEY = 'waYhg0HWEgd0mZKFCcVJ9UHPimJjjb4t';
 
 export default axios.create({
-  baseURL: 'http://api.giphy.com/v1/gifs',
+  baseURL: 'https://api.giphy.com/v1/gifs',
   params: {
     api_key: KEY,
     limit: 25
